51513dff8b16723ae86e92c286b4e8a81dcdc761
vieron <javiersanchezmarin@gmail.com>
2014-03-21 18:27:57 +0100

    chore: add tests

--- tests.js
+++ tests.js
@@ -4,6 +4,32 @@ var box = function(cls) {
     return '<div class="box-' + cls + '" />';
 };
 
+var checkAllTransEndEvents = function(transLength) {
+    it('should fire `trans:end events for each transitioned attribute using #addTransClass', function(done) {
+        this.timeout(3000);
+        var _done = _.after(transLength || 4, done.bind(this));
+
+        this.$box.on('trans:end', function() {
+            _done();
+        });
+
+        this.$box.addTransClass('js-active');
+    });
+
+    it('should fire `trans:end events for each transitioned attribute using #removeTransClass', function(done) {
+        this.timeout(3000);
+        this.$box.addClass('js-active');
+        var _done = _.after(transLength || 4, done.bind(this));
+
+        this.$box.on('trans:end', function() {
+            _done();
+        });
+
+        this.$box.removeTransClass('js-active');
+    });
+};
+
+
 describe('trans.js', function() {
     'use strict';
 
@@ -24,16 +50,7 @@ describe('trans.js', function() {
         expect(ins).to.respondTo('transition');
     });
 
-    it('should fire `trans:end events for each transitioned attribute', function(done) {
-        this.timeout(3000);
-        var _done = _.after(4, done.bind(this));
-
-        this.$box.on('trans:end', function() {
-            _done();
-        });
-
-        this.$box.addTransClass('js-active');
-    });
+    checkAllTransEndEvents.call(this);
 
     it('should fire `trans:end:propKey` events', function(done) {
         this.timeout(3000);
@@ -54,4 +71,125 @@ describe('trans.js', function() {
 
         this.$box.addTransClass('js-active');
     });
+});
+
+
+
+describe('trans.js opacity + display block/none', function() {
+    'use strict';
+
+    beforeEach(function() {
+        this.$box = $(box('opacity-display-trans'));
+        $('#fixture').append(this.$box);
+    });
+
+    afterEach(function() {
+        this.$box.data('trans').destroy();
+        this.$box.remove();
+    });
+
+    it('should set display property to none', function(done) {
+        this.$box.on('trans:endAll', function(e, ins) {
+            expect(ins.$el.css('display')).to.equal('none');
+            done();
+        });
+
+        this.$box.addTransClass('js-active');
+    });
+
+    it('should set display property to block', function(done) {
+        this.$box.addClass('js-active');
+
+        this.$box.on('trans:endAll', function(e, ins) {
+            expect(ins.$el.css('display')).to.equal('block');
+            done();
+        });
+
+        this.$box.removeTransClass('js-active');
+    });
+
+    checkAllTransEndEvents.call(this);
+
+});
+
+
+describe('trans.js width + display block/none', function() {
+    'use strict';
+
+    beforeEach(function() {
+        this.$box = $(box('width-display-trans'));
+        $('#fixture').append(this.$box);
+    });
+
+    afterEach(function() {
+        this.$box.data('trans').destroy();
+        this.$box.remove();
+    });
+
+    it('should set display property to none and width to 0px', function(done) {
+        this.$box.on('trans:endAll', function(e, ins) {
+            console.log('trans_enddAll')
+            expect(ins.$el.css('display')).to.equal('none');
+            expect(ins.$el.css('width')).to.equal('0px');
+            done();
+        });
+
+        this.$box.addTransClass('js-active');
+    });
+
+    it('should set display property to block and width to original', function(done) {
+        this.$box.addClass('js-active');
+
+        this.$box.on('trans:endAll', function(e, ins) {
+            expect(ins.$el.css('display')).to.equal('block');
+            expect(ins.$el.css('width')).to.equal('200px');
+            done();
+        });
+
+        this.$box.removeTransClass('js-active');
+    });
+
+    checkAllTransEndEvents.call(this, 3);
+
+});
+
+
+describe('trans.js height + display block/none', function() {
+    'use strict';
+
+    beforeEach(function() {
+        this.$box = $(box('height-display-trans'));
+        $('#fixture').append(this.$box);
+    });
+
+    afterEach(function() {
+        this.$box.data('trans').destroy();
+        this.$box.remove();
+    });
+
+    it('should set display property to none and height to 0px', function(done) {
+        this.$box.on('trans:endAll', function(e, ins) {
+            console.log('trans_enddAll')
+            expect(ins.$el.css('display')).to.equal('none');
+            expect(ins.$el.css('height')).to.equal('0px');
+            done();
+        });
+
+        this.$box.addTransClass('js-active');
+    });
+
+    it('should set display property to block and height to original', function(done) {
+        this.$box.addClass('js-active');
+
+        this.$box.on('trans:endAll', function(e, ins) {
+            expect(ins.$el.css('display')).to.equal('block');
+            expect(ins.$el.css('height')).to.equal('400px');
+            done();
+        });
+
+        this.$box.removeTransClass('js-active');
+    });
+
+    checkAllTransEndEvents.call(this, 3);
+
 });

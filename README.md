3b12f529723fc81ac222b6d62508c742749dc438
vieron <javiersanchezmarin@gmail.com>
2015-01-25 02:17:11 +0100

    complete refactor

--- README.md
+++ README.md
@@ -1,9 +1,66 @@
-# trans.js
-
-Cross-browser CSS transitions
-
 [![Build Status](https://travis-ci.org/vieron/trans.js.png?branch=master)](https://travis-ci.org/vieron/trans.js)
 
+[![Sauce Test Status](https://saucelabs.com/browser-matrix/transjs.svg)](https://saucelabs.com/u/transjs)
+
+# trans.js
+
+Cross-browser CSS transitions.
+
+Trans.js is a small JavaScript library that allows you manage CSS transitions adding or removing CSS classes previously defined by you in your stylesheets.
+
+**Why another?**
+
+Yes, there is a lot of alternatives to manage CSS transitions, like [gsap](https://greensock.com/gsap), [Velocity.js](http://julian.com/research/velocity/), [jquery.transit](http://ricostacruz.com/jquery.transit/)...
+
+The main goal of Trans.js it's to keep your transitions logic in the CSS, not spreading your javascript with a lot of appearance-related code.
+
+A visual example, trans.js let's you write this:
+
+```css
+// In your CSS
+.t-fade {
+    width: 400px;
+    opacity: 1;
+}
+
+.t-fade.transition {
+    transition: width 0.2s ease-in, opacity 0.6s ease-out;
+}
+
+.t-fade.is-hidden {
+    width: 100px;
+    opacity: 0;
+}
+```
+
+```js
+// In your JS
+Trans.addTransClass(el, 'is-hidden', function() {
+    // transitionEnd callback
+});
+```
+
+Instead of this, with hardcoded values in our CSS:
+
+```js
+// pseudo-code
+el.transition({
+    width: '100px',
+    opacity: 0,
+    easing: {width: 'ease-in', opacity: 'ease-out'},
+    duration: {width: 0.2, opacity: 0.6}
+}, function() {
+    // transitionEnd callback
+});
+```
+
+## Features
+
+* Manage transitions from your CSS.
+* It handles transitions automatically in elements with display none/block.
+* It handles transitions automatically in elements with width auto/0 or height auto/0.
+* Cross-browser transitionEnd callbacks, per property or at the end of the whole transition.
+* Customizable after/before transition hooks.
 
 ## Installation
 
@@ -12,177 +69,33 @@ Install with bower:
     $ bower install trans.js
 
 
-## Features
+## Usage
 
-* It handles well transitions between elements with the following properties:
-    * `display: none;`  <=>  `display: block;`
-    * `height: 0;`      <=>  `height: auto;`
-    * `width: 0;`       <=>  `width: auto;`
-    * `opacity: 0;`     <=>  `opacity: 1;`
-
-* It handles events not fired in transitions between equal values...
-* TransitionEnd callbacks and events, by property or at the end of all transitions
-* Fallback to addClass/removeClass when transitions aren't supported. All transitionEnd
-events and callbacks binded will be fired.
-
-
-## Options
+### Usage with jQuery
 
 ```js
-{
-    end: function(propKey, instance) {}, // fired at the end of transition of each property
-    endAll: function(propKey, instance) {} // fired once at the end of all transitions
+// arg1 = className
+// arg2 = endAllCallback or config object
+// arg3 = featureTest
+$.fn.removeTransClass = function(arg1, arg2, arg3) {
+    return this.each(function() {
+        Trans.removeTransClass.apply(null, [this, arg1, arg2, arg3]);
+    });
+}
+
+$.fn.addTransClass = function(arg1, arg2, arg3) {
+    return this.each(function() {
+        Trans.addTransClass.apply(null, [this, arg1, arg2, arg3]);
+    });
 }
 ```
 
-## API
-
-Add or remove a class on a element and fires transitionEnd callbacks.
-
-* `className`: *(string)* The class or group of classes separated by commas to add or remove.
-* `callabckFn`: *(function)* Callback function fired at the end of all transitions.
-* `options`: *(object)* Object width options.
-* `fallbackTest`: *(Boolean)* Apply fallback logic whenever you want with a custom fallback test.
-
-### `.addTransClass(className [, callbackFn || options || fallbackTest, fallbackTest])`
-
-```js
-$('#foo').addtransClass('.js-flip', function() {
-    console.log('all transitions finished');
-});
-
-// Use a custom fallback test
-$('#foo').addtransClass('.js-flip', function() {
-    console.log('all transitions finished');
-}, Modernizr.csstransforms3d);
-
-// Pass fallbackTest as second argument if you don't need a callback
-$('#foo').addtransClass('.js-flip', Modernizr.csstransforms3d);
-```
-
-### `.removeTransClass(className [, callbackFn || options || fallbackTest, fallbackTest])`
-
-```js
-$('#foo').removetransClass('.js-flip', function() {
-    console.log('all transitions finished');
-});
-
-// Use a custom fallback test
-$('#foo').removetransClass('.js-flip', function() {
-    console.log('all transitions finished');
-}, Modernizr.csstransforms3d);
-
-// Pass fallbackTest as second argument if you don't need a callback
-$('#foo').removetransClass('.js-flip', Modernizr.csstransforms3d);
-```
-
-## Events
-
-You can subscribe to the following events:
-
-### `trans:end`
-
-```js
-$('#foo').on('trans:end', function(propKey, transInstance) {
-    console.log('end of ', propKey, ' transition');
-});
-
-$('#foo').addTransClass('js-active');
-```
-
-### `trans:end:[propName]`
-
-```js
-$('#foo').on('trans:end:background-color', function(propKey, transInstance) {
-    console.log('end of background color transition');
-});
-
-$('#foo').addTransClass('js-active');
-```
-
-### `trans:endAll`
-
-```js
-$('#foo').on('trans:endAll', function(transInstance) {
-    console.log('end of all transitions');
-});
-
-$('#foo').addTransClass('js-active');
-```
+If you need to support IE9 include the [classList pollyfill](https://github.com/eligrey/classList.js) before trans.js is loaded.
 
 
-## Utilities
-
-Note that this utilities do not handle transitions between not transitionable
-attributes listed above.
-
-Do not provide any kind of fallback.
-
-### `.transEnd(callbackFn [, context || fallbackTest, fallbackTest])`
-
-```js
-$('#foo').transEnd(function(prop) {
-    console.log('transEnd method ' + prop);
-});
-
-$('#foo').addClass('js-active');
-```
-
-or using a custom fallback
-
-```js
-$('#foo').transEnd(function(prop) {
-    console.log('transEnd method ' + prop);
-}, Modernizr.csstransforms3d);
-```
-
-### `transEnd` jQuery event type
-
-```js
-$('#foo').on('transEnd', function(e) {
-    console.log('transEnd method', e.originalEvent.propertyName);
-});
-
-$('#foo').addClass('js-active');
-
-// remember unbind the event
-$('#foo').off('transEnd');
-
-```
-
-### `.noTrans(callbackFn [, context])`
-
-Perform changes in CSS attributes that have CSS transitions applied without transitioning them.
-
-```js
-$('#foo').noTrans(function(prop) {
-    // all changes here will be done without transitions
-    $('#foo').css('height', 200);
-    $('#foo').css('width', 500);
-});
-```
-
-
-
-## Known problems
-
-* Do not transition shorthand properties like background, use expanded properties instead: background-color, background-position...
-
-## Browser support
-
-* Safari
-* Chrome
-* Opera
-* Firefox
-* IE9 +
 
 ## TODO
 
-* trans.js uses display block instead of the display property value of the element (fix in next version)
-* un-vendor prefix property keys used in transProps and in `trans:end:propKey` events.
-* API to enqueue transitions
-* implement promises with $.Deferred
+Read TODO comments over the code
 
-## License
 
-  MIT
--- tests.js
+++ tests.js
@@ -1,246 +1,356 @@
+'use strict';
+
 var expect = chai.expect;
 
-var box = function(cls) {
-    return '<div class="box-' + cls + '" />';
+var defer = function(fn, ctx) {
+    setTimeout(fn, 0);
+}
+
+var createElement = function(cls) {
+    this.fixture = document.querySelector('#fixture');
+    this.box = document.createElement("div");
+    this.box.classList.add('box-' + cls);
+    this.fixture.appendChild(this.box);
+    return this.box;
 };
 
-var checkAllTransEndEvents = function(transLength) {
-    it('should fire `trans:end events for each transitioned attribute using #addTransClass', function(done) {
-        this.timeout(3000);
-        var _done = _.after(transLength || 4, done.bind(this));
+var removeElement = function() {
+    this.fixture.removeChild(this.box);
+}
 
-        this.$box.on('trans:end', function() {
-            _done();
-        });
+var css = function(el, prop) {
+    var style = window.getComputedStyle(el, null);
+    return style.getPropertyValue(prop);
+}
 
-        this.$box.addTransClass('js-active');
-    });
+var lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quaerat dignissimos unde, nobis, fugit inventore quae facilis enim nemo nisi exercitationem voluptas pariatur ut magni officia aliquam asperiores? Omnis, iusto.';
 
-    it('should fire `trans:end events for each transitioned attribute using #removeTransClass', function(done) {
-        this.timeout(3000);
-        this.$box.addClass('js-active');
-        var _done = _.after(transLength || 4, done.bind(this));
-
-        this.$box.on('trans:end', function() {
-            _done();
-        });
-
-        this.$box.removeTransClass('js-active');
-    });
-};
+// add #fixture div when running tests with karma
+if (document.body) {
+    var fixture = document.createElement('div'); fixture.setAttribute('id', 'fixture');
+    document.querySelector('#fixture') || document.body.appendChild(fixture);
+}
 
 
 describe('trans.js', function() {
-    'use strict';
 
-    beforeEach(function() {
-        this.$box = $(box('regular-trans'));
-        $('#fixture').append(this.$box);
-    });
-
-    afterEach(function() {
-        this.$box.data('trans').destroy();
-        this.$box.remove();
-    });
-
-    it('should return an instance of Trans when calling data("trans")', function() {
-        this.$box.addTransClass('js-active');
-        var ins = this.$box.data('trans');
-        expect(ins).to.be.instanceof(ins.constructor);
-        expect(ins).to.respondTo('transition');
-    });
-
-    checkAllTransEndEvents.call(this);
-
-    it('should fire `trans:end:propKey` events', function(done) {
-        this.timeout(3000);
-
-        this.$box.on('trans:end:background-color', function() {
-            done();
+    describe('transEnd callback', function() {
+        beforeEach(function() {
+            createElement.call(this, 'regular-trans');
         });
 
-        this.$box.addTransClass('js-active');
-    });
-
-    it('should fire `trans:endAll` event when all transitions finish', function(done) {
-        this.timeout(3000);
-
-        this.$box.on('trans:endAll', function() {
-            done();
+        afterEach(function() {
+            removeElement.call(this);
         });
 
-        this.$box.addTransClass('js-active');
+        it('should fire `endAll` callback when the whole transition finished', function(done) {
+            this.timeout(6000);
+            Trans.transEndAll(this.box, function() {
+                Trans.transEndAll(this.box, function() {
+                    done();
+                }.bind(this));
+                this.box.classList.remove('js-active');
+            }.bind(this));
+            this.box.classList.add('js-active');
+        });
     });
+
+
+    describe('regular transition', function() {
+        beforeEach(function() {
+            createElement.call(this, 'regular-trans');
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should fire `end` callback per transitioned property', function(done) {
+            this.timeout(2000);
+            var _done = _.after(4, done);
+            var transitionedProps = ['background-color', 'width', 'height', 'transform', '-webkit-transform'];
+
+            Trans.addTransClass(this.box, 'js-active', {
+                end: function(el, propKey) {
+                    // for browsers not supporting css transitions
+                    if (propKey === 'all') {
+                        done();
+                    }
+                    // for the rest of browsers
+                    else {
+                        expect(transitionedProps).to.contain(propKey);
+                        _done();
+                    }
+                }
+            });
+        });
+
+        it('should fire `endAll` callback when the whole transition transitions finish', function(done) {
+            this.timeout(4000);
+
+            // passing endAll callback function as second argument and as config object
+            Trans.addTransClass(this.box, 'js-active', function() {
+                Trans.removeTransClass(this.box, 'js-active', {
+                    endAll: function() {
+                        done();
+                    }
+                });
+            }.bind(this));
+        });
+
+        it('should fire `end` and `endAll` callbacks with transitions disabled', function(done) {
+            var _done = _.after(4, done);
+
+            Trans.addTransClass(this.box, 'js-active', {
+                end: function() { _done(); },
+                endAll: function() {
+                    _done();
+
+                    Trans.removeTransClass(this.box, 'js-active', {
+                        end: function() { _done(); },
+                        endAll: function() { _done(); }
+                    }, false);
+                }.bind(this)
+            }, false);
+        });
+    });
+
+
+    describe('opacity + display block/none', function() {
+        beforeEach(function() {
+            createElement.call(this, 'opacity-display-trans');
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set display property to none', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('none');
+                done();
+            }.bind(this));
+        });
+
+        it('should set display property to block', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('block');
+                done();
+            }.bind(this));
+        });
+    });
+
+
+    describe('height auto/0', function() {
+        beforeEach(function() {
+            createElement.call(this, 'height-auto-trans');
+            // TODO: test without content
+            this.box.innerHTML = lipsum;
+            this.initialHeight = this.box.offsetHeight;
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set height to 0', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(this.box.offsetHeight).to.equal(0);
+                done();
+            }.bind(this));
+        });
+
+        it('should set height to the initial one', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(this.box.offsetHeight).to.equal(this.initialHeight);
+                done();
+            }.bind(this));
+        });
+    });
+
+
+    describe('width auto/0', function() {
+        beforeEach(function() {
+            createElement.call(this, 'width-auto-trans');
+            // TODO: test without content
+            this.box.innerHTML = lipsum;
+            this.initialWidth = this.box.offsetWidth;
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set width to 0', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(this.box.offsetWidth).to.equal(0);
+                done();
+            }.bind(this));
+        });
+
+        it('should set width to the initial one', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(this.box.offsetWidth).to.equal(this.initialWidth);
+                done();
+            }.bind(this));
+        });
+    });
+
+
+    describe('width + display block/none', function() {
+        beforeEach(function() {
+            createElement.call(this, 'width-display-trans');
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set display property to none and width to 0px', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('none');
+                expect(css(this.box, 'width')).to.equal('0px');
+                done();
+            }.bind(this));
+        });
+
+        it('should set display property to block and width to original', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('block');
+                expect(css(this.box, 'width')).to.equal('200px');
+                done();
+            }.bind(this));
+        });
+    });
+
+
+    describe('height auto/0 + display block/none', function() {
+        beforeEach(function() {
+            createElement.call(this, 'height-auto-display-trans');
+            this.box.innerHTML = lipsum;
+            this.initialHeight = this.box.offsetHeight;
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set display property to none and height to 0px', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('none');
+                expect(this.box.offsetHeight).to.equal(0);
+                done();
+            }.bind(this));
+        });
+
+        it('should set display property to block and height to original', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('block');
+                expect(this.box.offsetHeight).to.equal(this.initialHeight);
+                done();
+            }.bind(this));
+        });
+    });
+
+
+    describe('width auto/0 + display block/none', function() {
+        beforeEach(function() {
+            createElement.call(this, 'width-auto-display-trans');
+            this.box.innerHTML = lipsum;
+            this.initialWidth = this.box.offsetWidth;
+        });
+
+        afterEach(function() {
+            removeElement.call(this);
+        });
+
+        it('should set display property to none and width to 0px', function(done) {
+            Trans.addTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('none');
+                expect(this.box.offsetWidth).to.equal(0);
+                done();
+            }.bind(this));
+        });
+
+        it('should set display property to block and width to original', function(done) {
+            this.box.classList.add('js-active');
+
+            Trans.removeTransClass(this.box, 'js-active', function() {
+                expect(css(this.box, 'display')).to.equal('block');
+                expect(this.box.offsetWidth).to.equal(this.initialWidth);
+                done();
+            }.bind(this));
+        });
+    });
+
 });
 
 
 
-describe('trans.js opacity + display block/none', function() {
-    'use strict';
-
-    beforeEach(function() {
-        this.$box = $(box('opacity-display-trans'));
-        $('#fixture').append(this.$box);
-    });
-
-    afterEach(function() {
-        this.$box.data('trans').destroy();
-        this.$box.remove();
-    });
-
-    it('should set display property to none', function(done) {
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('none');
-            done();
-        });
-
-        this.$box.addTransClass('js-active');
-    });
-
-    it('should set display property to block', function(done) {
-        this.$box.addClass('js-active');
-
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('block');
-            done();
-        });
-
-        this.$box.removeTransClass('js-active');
-    });
-
-    checkAllTransEndEvents.call(this);
-
-});
 
 
-describe('trans.js width + display block/none', function() {
-    'use strict';
-
-    beforeEach(function() {
-        this.$box = $(box('width-display-trans'));
-        $('#fixture').append(this.$box);
-    });
-
-    afterEach(function() {
-        this.$box.data('trans').destroy();
-        this.$box.remove();
-    });
-
-    it('should set display property to none and width to 0px', function(done) {
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('none');
-            expect(ins.$el.css('width')).to.equal('0px');
-            done();
-        });
-
-        this.$box.addTransClass('js-active');
-    });
-
-    it('should set display property to block and width to original', function(done) {
-        this.$box.addClass('js-active');
-
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('block');
-            expect(ins.$el.css('width')).to.equal('200px');
-            done();
-        });
-
-        this.$box.removeTransClass('js-active');
-    });
-
-    checkAllTransEndEvents.call(this, 3);
-
-});
 
 
-describe('trans.js height + display block/none', function() {
-    'use strict';
 
-    beforeEach(function() {
-        this.$box = $(box('height-display-trans'));
-        $('#fixture').append(this.$box);
-    });
+// describe('utilities', function() {
+//     'use strict';
 
-    afterEach(function() {
-        this.$box.data('trans').destroy();
-        this.$box.remove();
-    });
+//     beforeEach(function() {
+//         this.$box = $(box('regular-trans'));
+//         $('#fixture').append(this.$box);
+//     });
 
-    it('should set display property to none and height to 0px', function(done) {
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('none');
-            expect(ins.$el.css('height')).to.equal('0px');
-            done();
-        });
+//     afterEach(function() {
+//         this.$box.remove();
+//     });
 
-        this.$box.addTransClass('js-active');
-    });
+//     it('should fire callback when using `transEnd` method', function(done) {
+//         this.timeout(3000);
 
-    it('should set display property to block and height to original', function(done) {
-        this.$box.addClass('js-active');
+//         this.$box.transEnd(function() {
+//             done();
+//         });
 
-        this.$box.on('trans:endAll', function(e, ins) {
-            expect(ins.$el.css('display')).to.equal('block');
-            expect(ins.$el.css('height')).to.equal('400px');
-            done();
-        });
+//         _.defer(function() {
+//             this.$box.addClass('js-active');
+//         }.bind(this));
+//     });
 
-        this.$box.removeTransClass('js-active');
-    });
+//     it('should fire callback when using `transEnd` jQuery event type', function(done) {
+//         this.timeout(3000);
 
-    checkAllTransEndEvents.call(this, 3);
-});
+//         this.$box.one('transEnd', function() {
+//             done();
+//         });
 
+//         _.defer(function() {
+//             this.$box.addClass('js-active');
+//         }.bind(this));
+//     });
 
-describe('trans.js utilities', function() {
-    'use strict';
+//     it('should change attributes without transitions using `noTrans` method', function() {
+//         var random_w = _.random(200, 500);
+//         var random_h = _.random(200, 500);
 
-    beforeEach(function() {
-        this.$box = $(box('regular-trans'));
-        $('#fixture').append(this.$box);
-    });
+//         // trigger repaint
+//         this.$box.width();
 
-    afterEach(function() {
-        this.$box.remove();
-    });
+//         this.$box.noTrans(function() {
+//             this.$box.css('width', random_w);
+//             this.$box.css('height', random_h);
+//         }, this);
 
-    it('should fire callback when using `transEnd` method', function(done) {
-        this.timeout(3000);
-
-        this.$box.transEnd(function() {
-            done();
-        });
-
-        _.defer(function() {
-            this.$box.addClass('js-active');
-        }.bind(this));
-    });
-
-    it('should fire callback when using `transEnd` jQuery event type', function(done) {
-        this.timeout(3000);
-
-        this.$box.one('transEnd', function() {
-            done();
-        });
-
-        _.defer(function() {
-            this.$box.addClass('js-active');
-        }.bind(this));
-    });
-
-    it('should change attributes without transitions using `noTrans` method', function() {
-        var random_w = _.random(200, 500);
-        var random_h = _.random(200, 500);
-
-        // trigger repaint
-        this.$box.width();
-
-        this.$box.noTrans(function() {
-            this.$box.css('width', random_w);
-            this.$box.css('height', random_h);
-        }, this);
-
-        expect(this.$box.width()).to.equal(random_w);
-        expect(this.$box.height()).to.equal(random_h);
-    });
-});
+//         expect(this.$box.width()).to.equal(random_w);
+//         expect(this.$box.height()).to.equal(random_h);
+//     });
+// });
